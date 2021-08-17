e1612ecd039d1b579a4522c9827048ebc44f5800
Matt Liberty <matt.liberty@jetperch.com>
2021-08-17 17:07:33 -0400

    Cleaned up capture_jls_v2 example.

--- bin/capture_jls_v2.py
+++ bin/capture_jls_v2.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Capture Joulescope samples to a JLS v2.  See https://github.com/jetperch/jls"""
+"""Capture Joulescope data to a JLS v2 file.  See https://github.com/jetperch/jls"""
 
 from joulescope import scan_require_one
 from pyjls import Writer, SourceDef, SignalDef, SignalType, DataType
@@ -25,9 +25,39 @@ import signal
 import time
 
 
+def _duration_validator(d):
+    if d is None or not len(d):
+        return None
+    if d[-1] == 's':
+        return float(d[:-1])
+    elif d[-1] == 'm':
+        return 60 * float(d[:-1])
+    elif d[-1] == 'h':
+        return 60 * 60 * float(d[:-1])
+    elif d[-1] == 'd':
+        return 60 * 60 * 24 * float(d[:-1])
+    else:
+        return float(d)
+
+
+def _sampling_rate_validator(s):
+    if isinstance(s, str):
+        n, u = s.split()
+        s = int(n)
+        if u[0] == 'M':
+            s *= 1000000
+        elif u[0] == 'k':
+            s *= 1000
+    return int(s)
+
+
 def get_parser():
     p = argparse.ArgumentParser(
         description='Capture Joulescope samples to a JLS v2.  See https://github.com/jetperch/jls')
+    p.add_argument('--duration',
+                   type=_duration_validator,
+                   help='The capture duration in float seconds. '
+                   + 'Add a suffix for other units: s=seconds, m=minutes, h=hours, d=days')
     p.add_argument('filename',
                    help='The JLS filename to record.')
     return p
@@ -36,13 +66,7 @@ def get_parser():
 def jls_open(args, device):
     info = device.info()
     sampling_rate = device.parameter_get('sampling_frequency')
-    if isinstance(sampling_rate, str):
-        n, u = sampling_rate.split()
-        sampling_rate = int(n)
-        if u[0] == 'M':
-            sampling_rate *= 1000000
-        elif u[0] == 'k':
-            sampling_rate *= 1000
+    sampling_rate = _sampling_rate_validator(sampling_rate)
 
     source = SourceDef(
         source_id=1,
@@ -97,7 +121,6 @@ class Process:
         # long-running operations will cause sample drops
         start_id, end_id = stream_buffer.sample_id_range
         if self._idx < end_id:
-            # only get and store what we need
             data = stream_buffer.samples_get(self._idx, end_id, fields=['current', 'voltage'])
             i = np.ascontiguousarray(data['signals']['current']['value'])
             v = np.ascontiguousarray(data['signals']['voltage']['value'])
@@ -109,14 +132,11 @@ class Process:
 def run():
     quit_ = False
     args = get_parser().parse_args()
+    duration = args.duration
 
     def do_quit(*args, **kwargs):
         nonlocal quit_
         quit_ = 'quit from SIGINT'
-
-    def on_stop(event, message):
-        nonlocal quit_
-        quit_ = 'quit from stop duration'
 
     signal.signal(signal.SIGINT, do_quit)
     device = scan_require_one(config='auto')
@@ -125,10 +145,13 @@ def run():
         p = Process(device, wr)
         try:
             device.stream_process_register(p)
+            t_stop = None if duration is None else time.time() + duration
             device.start()
             print('Capturing data: type CTRL-C to stop')
             while not quit_:
                 time.sleep(0.01)
+                if t_stop and time.time() > t_stop:
+                    break
             device.stop()
         finally:
             wr.close()
