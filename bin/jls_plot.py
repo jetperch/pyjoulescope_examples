fd424b093300e6d86fbd5fb156d8e7e0bb473c37
Matt Liberty <matt.liberty@jetperch.com>
2022-01-06 19:16:35 -0500

    Improved statistics formatting and made statistics display optional.

--- bin/jls_plot.py
+++ bin/jls_plot.py
@@ -4,10 +4,11 @@
 import argparse
 import numpy as np
 import sys
+import math
 import matplotlib.pyplot as plt
 from joulescope.data_recorder import DataReader
 from joulescope.view import data_array_to_update
-from joulescope.units import three_sig_figs
+from joulescope.units import unit_prefix
 
 
 # Developed for https://forum.joulescope.com/t/automation-of-plotting-long-term-records/415
@@ -20,6 +21,9 @@ def get_parser():
                    help='The input filename path.')
     p.add_argument('--out',
                    help='The output filename path.')
+    p.add_argument('--stats',
+                   action='store_true',
+                   help='Display statistics on the plot.')
     p.add_argument('--show',
                    action='store_true',
                    help='Display the plot.')
@@ -28,6 +32,52 @@ def get_parser():
                    default=1000,
                    help='The number of samples to display')
     return p
+
+
+# Statistics formatting copied from joulescope_ui.widgets.waveform.signal_statistics
+def _si_format(names, values, units):
+    results = []
+    if units is None:
+        units = ''
+    if len(values):
+        values = np.array(values)
+        max_value = float(np.max(np.abs(values)))
+        _, prefix, scale = unit_prefix(max_value)
+        scale = 1.0 / scale
+        if not len(prefix):
+            prefix = '&nbsp;'
+        units_suffix = f'{prefix}{units}'
+        for lbl, v in zip(names, values):
+            v *= scale
+            if abs(v) < 0.000005:  # minimum display resolution
+                v = 0
+            v_str = ('%+6f' % v)[:8]
+            results.append('%s=%s %s' % (lbl, v_str, units_suffix))
+    return results
+
+
+def si_format(labels):
+    results = []
+    if not len(labels):
+        return results
+    units = None
+    values = []
+    names = []
+    for name, d in labels.items():
+        value = float(d['value'])
+        if name == 'σ2':
+            name = 'σ'
+            value = math.sqrt(value)
+        if d['units'] != units:
+            results.extend(_si_format(names, values, units))
+            units = d['units']
+            values = [value]
+            names = [name]
+        else:
+            values.append(value)
+            names.append(name)
+    results.extend(_si_format(names, values, units))
+    return results
 
 
 def run():
@@ -44,19 +94,18 @@ def run():
     d = data_array_to_update(x_limits, x, data)
     s = r.statistics_get(start_idx, stop_idx)
     s_str = [f't = {x[-1]:.3} s']
-    for field, v in s['signals']['current'].items():
-        t = three_sig_figs(v['value'], v['units'])
-        s_str.append(f'{field} = {t}')
+    s_str += si_format(s['signals']['current'])
 
     f = plt.figure()
     ax_i = f.add_subplot(1, 1, 1)
-    f.subplots_adjust(right=0.8)
     ax_i.set_title('Current vs Time')
     ax_i.grid(True)
     ax_i.plot(x, d['signals']['current']['µ']['value'])
     ax_i.set_xlabel('Time (seconds)')
     ax_i.set_ylabel('Current (A)')
-    f.text(0.99, 0.85, '\n'.join(s_str), horizontalalignment='right', verticalalignment='top')
+    if args.stats:
+        f.subplots_adjust(right=0.75)
+        f.text(0.99, 0.85, '\n'.join(s_str), horizontalalignment='right', verticalalignment='top')
     if args.show:
         plt.show()
     if args.out:
